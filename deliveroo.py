bfd7c74b1ecbba191d48451c205fd0ae6f3173b4
Adyan Ehtesham <67388808+adyanehtesham@users.noreply.github.com>
2021-02-16 13:02:02 +0400

    first commit, finished scraping for questions, created mongodb

---
+++ deliveroo.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+
+URL = 'https://deliveroo.co.uk/faq'
+page = requests.get(URL)
+
+# The following creates a BeautifulSoup object  with the url from the page var.
+# You also tell it to use the appropriate parser
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# pprint.pprint(soup.prettify)
+
+# finding the appropriate element that has all the job listings using the id
+results = soup.find(class_='ccl-4c92e28b43e557eb ccl-85b2395f51210087')
+source = results.text
+
+# creating question and answer list and initialising title to be used in csv
+questions = []
+questions.append('Question')
+answers = []
+answers.append('Answer')
+
+faqQuestion = results.find_all('h3')
+
+for i in faqQuestion:
+    questions.append(i.text)
+
+for i in questions:
+    if i in source:
+        source = source.replace(i, 'REGULAREX')
+
+answerRegex = re.compile(r'EX(.*?)REGULAR', re.DOTALL)
+for i in answerRegex.findall(source):
+    answers.append(i)
+
+# creating a csv file and then adding the questions and answers
+with open('deliveroo.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(zip(questions, answers))
---
+++ uberEats.py
@@ -0,0 +1,61 @@
+# you can get the questions by using regex
+# each question starts with "QUESTION:" and ends with </br>
+# get the data and store it in a file
+# use regex to filter out everything else
+# keep "QUESTION:" so you can use regex or something to store qyestions and
+# their respective answers.
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+
+URL = 'https://help.uber.com/ubereats/article/dine-in-and-pick-up-order-faq?nodeId=dceda7b6-cc5a-4351-beae-b87ae7c28de9'
+page = requests.get(URL)
+
+# Creating a BeautifulSoup object
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# Element with all the faqs
+results = soup.find(class_='layout__item desk-and-up-four-fifths')
+
+# finds all the individual faq elements
+faqQuestion = results.find_all('span')
+
+# writing to a file
+text = open('uberEats.txt', 'w')
+for i in faqQuestion:
+    text.write(i.text + '\n')
+text.close()
+
+with open("uberEats.txt", 'r') as f:
+    uberEats_txt = f.readlines()
+whole_txt = "".join(uberEats_txt)
+
+# regex stuff, seperating questions and answers
+# creating question and answer list and initialising title to be used in csv
+questions = []
+questions.append('Question')
+answers = []
+answers.append('Answer')
+questionRegex = re.compile(r'QUESTION:.*\?')
+answerRegex = re.compile(r'\?(.*?)QUESTION', re.DOTALL)
+
+for i in questionRegex.findall(whole_txt):
+    questions.append(i)
+
+for i in answerRegex.findall(whole_txt):
+    answers.append(i)
+
+# removing uneccessasry items.
+for i in answers:
+    i = i.replace('\n', ' ')
+    i = i.replace('. . .', '')
+
+for i in questions:
+    i.replace("QUESTION:", '')
+
+# creating a csv file and then adding the questions and answers
+with open('uber.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(zip(questions, answers))
